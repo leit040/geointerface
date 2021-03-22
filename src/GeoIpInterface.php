d97226c76af9edc8837239bd9eb8aab0787215fc
Allen <allen@localhost>
2021-03-22 13:14:29 +0200

    v.0.0.1

---
+++ src/GeoIpInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Leit\Geo;
+
+//add pakages
+interface GeoIpInterface
+{
+    public function continentCode();
+
+    public function countryCode();
+
+    public function parse($ip);
+
+}
